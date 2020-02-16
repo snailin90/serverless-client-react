b0e2c89a383a92a9a42949d3a179510aae5cc526
Snailin Inoa <s.inoa@gbh.com.do>
2020-02-16 18:23:40 -0400

    Setting up Note React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-1",
+        BUCKET: "notes-apps-upload-sample"
+    },
+    apiGateway: {
+        REGION: "eu-west-1",
+        URL: "https://sji2gr61a4.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_PS3aAd5hG",
+        APP_CLIENT_ID: "18k0svtb9t5pt1eg3leb618rs2",
+        IDENTITY_POOL_ID: "eu-west-1:9ce52c6f-a701-484f-b704-61f6df7fce71"
+    }
+};
